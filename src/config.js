2e76728cbb545be9553cd77356d9b6663469698c
Vlad <40249712+vladikmihalko@users.noreply.github.com>
2019-08-14 08:12:47 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-// 	TOKEN: '821308876:AAFyZiSYjDYIDesWvY6VRzC4ACOjOdwRGec',
-	TOKEN: '963311797:AAELEuHQ6ihRyCqoDfhQ0FOJRl5i6b4BSS8', //orig
+ 	TOKEN: '78f6402a7ae4a7b8ad1fa2d0a226e2a2', //orig
 	QIWITOKEN: 'a3f8d9d40d021d7dfb7411a0893800e4',
 	QIWIPORTMONEY: '380931153618',
 	DB_URL:'mongodb+srv://VoiceFullinfobot:agryyrltwv_1998@voicefullinfobot-vai0h.mongodb.net/test?retryWrites=true&w=majority'
